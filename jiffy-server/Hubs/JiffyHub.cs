6537b08c441f626e655f06e757a53b5a23a3492c
Kaloyan Manev <kmanev073@hotmail.com>
2019-12-15 23:56:40 +0200

    WebRTC works!

--- jiffy-server/Hubs/JiffyHub.cs
+++ jiffy-server/Hubs/JiffyHub.cs
@@ -13,12 +13,27 @@ namespace jiffy_server.Hubs
 
         public async Task StartChatRequest(string friendUsername)
         {
+            if (Context.ConnectionId == friendUsername)
+            {
+                return;
+            }
+            
             await Clients.Client(friendUsername).SendAsync("startChatRequest", Context.ConnectionId);
         }
         
         public async Task AcceptChatRequest(string friendUsername)
         {
             await Clients.Client(friendUsername).SendAsync("startChatAccepted");
+        }
+
+        public async Task IceCandidate(string friendUsername, string candidate)
+        {
+            await Clients.Client(friendUsername).SendAsync("iceCandidate", candidate);
+        }
+
+        public async Task Sdp(string friendUsername, string description)
+        {
+            await Clients.Client(friendUsername).SendAsync("sdp", description);
         }
     }
 }
